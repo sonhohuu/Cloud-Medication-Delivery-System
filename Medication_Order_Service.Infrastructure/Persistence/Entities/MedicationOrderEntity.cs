3f3085f91c8f061e0a8e5bdc2548e34ce6a47bd8
sonhohuu <hhson365@gmail.com>
2025-05-26 16:43:43 +0700

    test git push

---
+++ Medication_Order_Service.Infrastructure/Persistence/Entities/MedicationOrderEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medication_Order_Service.Infrastructure.Persistence.Entities
+{
+    public class MedicationOrderEntity
+    {
+    }
+}
